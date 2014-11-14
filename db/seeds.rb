b2121d3bd50cf7135881f5a1a7f9e0fafb343017
amshane <allisonmandy90@gmail.com>
2014-11-13 23:24:50 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Location.create([
+    {near: 'Chelsea'}, 
+    {near: 'Chinatown'}, 
+    {near: 'Financial District'}, 
+    {near: 'Greenwich Village'}, 
+    {near: "Hell's Kitchen"}, 
+    {near: 'Kips Bay'}, 
+    {near: 'Lincoln Square'}, 
+    {near: 'Little Italy'}, 
+    {near: 'Lower East Side'}, 
+    {near: 'Meat Packing District'}, 
+    {near: 'Midtown East'},
+    {near: 'Murray Hill'}, 
+    {near: 'SoHo'}, 
+    {near: 'Tribeca'}, 
+    {near: 'Upper East Side'}, 
+    {near: 'Upper West Side'}, 
+    {near: 'West Village'}])
+
+Category.create([
+    {name: 'Bar', cat_id: '4bf58dd8d48988d116941735'}, 
+    {name: 'Beer Garden', cat_id: '4bf58dd8d48988d117941735'}, 
+    {name: 'Brewery', cat_id: '50327c8591d4c4b30a586d5d'}, 
+    {name: 'Cocktail Bar', cat_id: '4bf58dd8d48988d11e941735'},
+    {name: 'Dive Bar', cat_id: '4bf58dd8d48988d118941735'}, 
+    {name: 'Gay Bar', cat_id: '4bf58dd8d48988d1d8941735'},
+    {name: 'Hotel Bar', cat_id: '4bf58dd8d48988d1d5941735'}, 
+    {name: 'Karaoke Bar', cat_id: '4bf58dd8d48988d120941735'}, 
+    {name: 'Nightclub', cat_id: '4bf58dd8d48988d11f941735'},  
+    {name: 'Pub', cat_id: '4bf58dd8d48988d11b941735'}, 
+    {name: 'Speakeasy', cat_id: '4bf58dd8d48988d1d4941735'}, 
+    {name: 'Sports Bar', cat_id: '4bf58dd8d48988d11d941735'}, 
+    {name: 'Wine Bar', cat_id: '4bf58dd8d48988d123941735'}])
